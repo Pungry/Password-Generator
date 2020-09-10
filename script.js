8ebf64b5f176178801e275bdd945257f4a72d88c
Quint Turner <henryquintturner@gmail.com>
2020-09-10 00:29:58 -0700

    Coded the initialization of the password generator from choosing the password length to the types of characters wanted for the password.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,6 +13,68 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Arrays of characters that'll be needed for looping through with functions. Goal is to write code that'll randomly take one element from an array the user requested, add it to a string with the push.password. This should loop until the desired length of the password is finished, i.e.: i = 0; i < passwordLength.length(); i++
+var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialCharacters = ["!", "#", "$", "%", "&", "(", ")", "<", ">", "*", "=", "-", "?"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//Code that prompts the user to input the length of the password and then stores that value as an integer
+var passwordLength = parseInt(prompt("Choose a length of password between 8 and 128"));
+console.log("Password Length: " + passwordLength);
+
+//Code that checks if the length is between 8 and 128 characters. Does not continue code if input is invalid. Last conditional should catch any non-numerical inputs, but doesn't work right now.
+if (passwordLength < 8 || passwordLength > 128 || passwordLength === NaN)
+  {alert("Input invalid. Choose a length between 8 and 128.");}
+
+else 
+  {
+    alert("Input valid. Creating password of " + passwordLength + " characters.");
+    
+    //Getting boolean values here to send to the functions that'll run the functions that actually generate characters for the password
+    var lowercaseUse = confirm("Do you want lowercase characters in your password?");
+    var uppercaseUse = confirm("Do you want uppercase characters in your password?");
+    var specialCharUse = confirm("Do you want special characters in your password?");
+    var numberUse = confirm("Do you want numbers in your password?");
+
+    //Checking if the user even gave the okay to using one type of character
+    if (lowercaseUse === false && uppercaseUse === false && specialCharUse === false && numberUse === false)
+      {
+        alert("You cannot make a character-based password if you do not choose any characters to use in your password.");
+      }
+    //Trying to write code that validates the user's input as workable and indicate what characters they've chosen to fill their password with. Not sure how to do that efficiently.
+    if (lowercaseUse === true || uppercaseUse === true || specialCharUse === true || numberUse === true)
+      {
+        alert("Duly noted. You would like a password with: ");
+        if (lowercaseUse === true)
+        {
+          alert("Lowercase Characters");
+        }
+        if (uppercaseUse === true)
+        {
+          alert("Uppercase Characters");
+        }
+        if (specialCharUse === true)
+        {
+          alert("Special Characters");
+        }
+        if (numberUse === true)
+        {
+          alert("Numbers");
+        }
+      //Here is where I should invoke the functions that go through the arrays of said characters.
+
+      }
+    
+  }
+
+//Create arrays for all uppercase chars, lowercase chars, special chars, and numbers
+//Have a loop go through all the arrays and select one element randomly
+//Create an array the same length that the user wants it to be, fill it with 0 chars until it is the length
+//Use a bunch of functions:
+//One function at start takes the integer input and then runs it through that loop to create the empty array which is the value returned
+//Four separate functions that loop through the different arrays of characters
 
 
 // //GIVEN I need a new, secure password
@@ -27,7 +90,9 @@ generateBtn.addEventListener("click", writePassword);
 //-give prompt that asks for length of password; make sure they input something between 8 and 128
 // i.e. var length = prompt("How long do you want the password to be? Must be between 8 and 128 characters")
 // if (parseInt(length) < 8 || parseInt(length) > 128)
-  //  {go back to the start of question}
+  //  print "not a valid length. input a length between 8 and 128 characters for the password" and end the code
+//or if (parseInt(length) === NaN)
+  //  print "not a number" and end the code
 
 // WHEN prompted for character types to include in the password
 // THEN I choose lowercase, uppercase, numeric, and/or special characters
