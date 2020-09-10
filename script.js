c40df94a50b814d7d5ab2fd3a6d6eb857048d84d
Quint Turner <henryquintturner@gmail.com>
2020-09-10 14:23:24 -0700

    Finished full functionality of password application. Still have yet to update Readme

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  console.log(password);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -20,12 +21,59 @@ var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"
 var specialCharacters = ["!", "#", "$", "%", "&", "(", ")", "<", ">", "*", "=", "-", "?"];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+//Initializing my password variable as a blank string before I start adding to it
+var newPassword = "";
+
+//Functions for looping through the arrays based on which characters were chosen. Each function should return a string value with the attached character.
+function lowercaseChoice() {
+  //console.log(newPassword);
+  newChar = lowercaseCharacters[Math.floor(Math.random() * 26)];
+  console.log(newChar);
+  newPassword += newChar;
+  //console.log(newPassword);
+  return newPassword;
+}
+
+function uppercaseChoice() {
+  //console.log(newPassword);
+  newChar = uppercaseCharacters[Math.floor(Math.random() * 26)];
+  console.log(newChar);
+  newPassword += newChar;
+  //console.log(newPassword);
+  return newPassword;
+}
+
+function specialChoice() {
+  //console.log(randomSpecial);
+  newChar = specialCharacters[Math.floor(Math.random() * 13)];
+  console.log(newChar);
+  newPassword += newChar;
+  //console.log(randomSpecial);
+  return newPassword;
+}
+
+function numberChoice() {
+  //console.log(randomNumber);
+  newChar = numbers[Math.floor(Math.random() * 10)];
+  console.log(newChar);
+  newPassword += newChar;
+  //console.log(randomNumber);
+  return newPassword;
+}
+
 //Code that prompts the user to input the length of the password and then stores that value as an integer
+function generatePassword()
+{
+
+
 var passwordLength = parseInt(prompt("Choose a length of password between 8 and 128"));
 console.log("Password Length: " + passwordLength);
 
-//Code that checks if the length is between 8 and 128 characters. Does not continue code if input is invalid. Last conditional should catch any non-numerical inputs, but doesn't work right now.
-if (passwordLength < 8 || passwordLength > 128 || passwordLength === NaN)
+if (typeof passwordLength === NaN)
+  {alert("Input invalid. Choose a numerical length.");}
+
+//Code that checks if the length is between 8 and 128 characters. Does not continue code if input is invalid. Last conditional should catch any non-numerical inputs, but doesn't fully stop the function right now.
+if (passwordLength < 8 || passwordLength > 128)
   {alert("Input invalid. Choose a length between 8 and 128.");}
 
 else 
@@ -64,11 +112,39 @@ else
           alert("Numbers");
         }
       //Here is where I should invoke the functions that go through the arrays of said characters.
+        for (var i = 0; i < passwordLength; i++)
+          {
+            //Chooses a random number between 0 and 3 to determine randomly what should be the next character in the password. Each time the loop happens, the number gets reset.
+            var charChoice = Math.floor((Math.random() * 4));
 
+            if (charChoice === 0 && lowercaseUse === true)
+            {console.log("lowercase Use + 1" + i);
+            lowercaseChoice();}
+
+            else if (charChoice === 1 && uppercaseUse === true)
+            {console.log("uppercase Use + 1" + i);
+            uppercaseChoice();}
+
+            else if (charChoice === 2 && specialCharUse === true)
+            {console.log("special char Use + 1" + i);
+            specialChoice();}
+
+            else if (charChoice === 3 && numberUse === true)
+            {console.log("numbers Use + 1" + i);
+            numberChoice();}
+
+            //If the random number generated is not from one of the character sets the user wants, i is decreased in order to run the loop the proper number of times to reach the correct password length.
+            else 
+            {
+              i--;
+            }
+          }
+        console.log(newPassword);
       }
     
   }
-
+  return newPassword;
+}
 //Create arrays for all uppercase chars, lowercase chars, special chars, and numbers
 //Have a loop go through all the arrays and select one element randomly
 //Create an array the same length that the user wants it to be, fill it with 0 chars until it is the length
